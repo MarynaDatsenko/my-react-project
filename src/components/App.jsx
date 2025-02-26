6767cfc2ef77dc6225be9fdb4c30e549ef680dad
marynad <mskameneva@gmail.com>
2025-02-26 11:13:31 +0200

    pract accordion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,42 @@
-import { useState, useEffect } from "react";
+import Accordion from "./Accordion/Accordion";
+import css from "./App.module.css";
 
-const App = () => {
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
-    if (savedClicks === null) {
-      return Number(savedClicks);
-    }
-    return 0;
-  });
+const accordionItems = [
+  {
+    title: "How does an accordion work?",
+    content:
+      "An accordion allows users to toggle the display of sections of content.",
+  },
+  {
+    title: "What are the benefits of using an accordion?",
+    content:
+      "Accordions are useful for organizing content and reducing clutter on a page.",
+  },
+  {
+    title: "Can an accordion be styled to match a website's design?",
+    content:
+      "Yes, accordions can be styled using CSS to match the design of a website.",
+  },
 
-  useEffect(() => {
-    window.localStorage.setItem("saved-clicks", clicks);
-  }, [clicks]);
+  {
+    title: "How to create an accordion in React?",
+    content:
+      "To create an accordion in React, you can use state to manage the open and closed state of each section.",
+  },
 
+  {
+    title: "What is the state in React?",
+    content:
+      "State is a built-in feature in React that allows components to manage and update their own data.",
+  },
+];
+
+export default function App() {
   return (
-    <div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
-      <button onClick={() => setClicks(0)}>Reset</button>
+    <div className={css.container}>
+      <h1 className={css.title}>State in React</h1>
+
+      <Accordion items={accordionItems} />
     </div>
   );
-};
-
-export default App;
+}
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+
+  server: {
+    open: true,
+  },
 });
