b57b876af567fe4165a32a2d06a8ec57ec47f8d4
marynad <mskameneva@gmail.com>
2025-03-04 15:38:18 +0200

    lang switcher

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,19 @@
 //import css from "./App.module.css";
 //import Reader from "./Reader/Reader";
 //import articles from "../components/articles.json";
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
+//import LoginForm from "./LoginForm";
+//import SearchBar from "./SearchBar";
+import LangSwitcher from "./LangSwitcher";
+import { useState } from "react";
+import "./styles.css";
 
-export default function ThemeSwitcher() {
-  const [isDark, setDark] = useState(
-    window.matchMedia("(prefers-color-scheme: dark")
+export default function App() {
+  const [lang, setLang] = useState("uk");
+  return (
+    <>
+      <p>Selected language: {lang} </p>
+      <LangSwitcher value={lang} onSelect={setLang} />
+    </>
   );
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-    const handleChange = () => setIsDark(mediaQuery.matches);
-
-    mediaQuery.addEventListener("change", handleChange);
-
-    return () => {
-      mediaQuery.removeEventListener("change", handleChange);
-    };
-  }, []);
-
-  return <div>{isDark ? "Темна тема" : "Світла тема"}</div>;
 }
