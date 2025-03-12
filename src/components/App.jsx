5eddfa14e51d9eb4a8e7936f140dbaf65810b61f
marynad <mskameneva@gmail.com>
2025-03-12 09:22:11 +0200

    lesson four p one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
-//import { useState } from "react";
-//import LoginForm from "./LoginForm";
-import FeedbackForm from "./FeedbackForm";
+import { useState } from "react";
+//import axios from "axios";
+import ArticleList from "./ArticleList";
+import { fetchArticlesWithTopic } from "../articles-api";
+import { SearchForm } from "./SearchForm";
 
-export default function App() {
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <FeedbackForm />
+      <h1>Latest articles</h1>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <Error />}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
-}
+};
+
+export default App;
